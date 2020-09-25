e3ea3631d3f9a839db6dc6ea97872e8d9038d9b2
Linoy Cohen <45672703+linoyco@users.noreply.github.com>
2020-09-25 17:45:12 +0300

    create mongoDB connection

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
-
 const adminRoutes = require('./routes/admin');
 const userRoutes = require('./routes/user');
 const loginRoutes = require('./routes/login');
 const freeRoutes = require('./routes/free');
+const mongoConnect = require('./lib/db');
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -19,4 +20,8 @@ app.use((req, res, next) => {
     res.status(404).send('<h1>Page not found...!!</h1>');
 });
 
-app.listen(3000);
+
+mongoConnect(client => {
+    console.log(client);
+    app.listen(3000);
+});
